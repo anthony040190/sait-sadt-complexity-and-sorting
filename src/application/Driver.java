9b7ea8602079b9a155f0c67c481e6fa986e7f673
JeanXristianPascua <119911259+JeanXristianPascua@users.noreply.github.com>
2023-10-18 01:35:53 -0600

    Add Code

--- src/application/Driver.java
+++ src/application/Driver.java
@@ -1,77 +1,67 @@
 package application;
 
-import shape.*;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
+import shape.*;
+
 public class Driver {
 	
-	 public static void main(String[] args) {
-		 
-		 String[] fileNames = {"res/polyfor1.txt", "res/polyfor3.txt", "res/polyfor5.txt"};
+    public static void main(String[] args) {
+        List<Shape> shapes = new ArrayList<>();
 
-		 List<Shape> shapes = new ArrayList<>();
-		 List<Cone> cone = new ArrayList<>();
-		 
-		 for(String fileName : fileNames) {
-			 
-			 File textFile = new File(fileName);
-			 
-			 try {
-				Scanner sc = new Scanner(textFile);
-				
-				while(sc.hasNextLine()) {
-					
-					String data = sc.nextLine();
-					String[] splittedData = data.split(" ");
-					int numberOfData = Integer.parseInt(splittedData[0]);
-					String shapeType = splittedData[1];
-	                double height = Double.parseDouble(splittedData[2]);
-					
+        String[] fileNames = {"res/polyfor1.txt", "res/polyfor3.txt", "res/polyfor5.txt"}; 
+
+        for (String fileName : fileNames) {
+            try {
+                File file = new File(fileName);
+                Scanner scanner = new Scanner(file);
+
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    String[] parts = line.split(" "); 
+                    Long data = Long.parseLong(parts[0]);
+                    String shapeType = parts[1];
+                    double height = Double.parseDouble(parts[2]);
+
                     if (shapeType.equals("Cylinder")) {
-                        double radius = Double.parseDouble(splittedData[3]);
+                        double radius = Double.parseDouble(parts[3]);
                         shapes.add(new Cylinder(height, radius));
                     } else if (shapeType.equals("Cone")) {
-                        double radius = Double.parseDouble(splittedData[3]);
+                        double radius = Double.parseDouble(parts[3]);
                         shapes.add(new Cone(height, radius));
                     } else if (shapeType.equals("Pyramid")) {
-                        double side = Double.parseDouble(splittedData[3]);
+                        double side = Double.parseDouble(parts[3]);
                         shapes.add(new Pyramid(height, side));
                     } else if (shapeType.equals("PentagonalPrism")) {
-                        double side = Double.parseDouble(splittedData[3]);
+                        double side = Double.parseDouble(parts[3]);
                         shapes.add(new Pyramid(height, side));
                     } else if (shapeType.equals("TriangularPrism")) {
-                        double side = Double.parseDouble(splittedData[3]);
+                        double side = Double.parseDouble(parts[3]);
                         shapes.add(new Pyramid(height, side));
                     } else if (shapeType.equals("OctagonalPrism")) {
-                        double side = Double.parseDouble(splittedData[3]);
+                        double side = Double.parseDouble(parts[3]);
                         shapes.add(new Pyramid(height, side));
                     } else if (shapeType.equals("SquarePrism")) {
-                        double side = Double.parseDouble(splittedData[3]);
+                        double side = Double.parseDouble(parts[3]);
                         shapes.add(new Pyramid(height, side));
                     }
-  					
-				
-				}
+                scanner.close();
+                }
+                
+                } catch (FileNotFoundException e) {
+                System.out.println("File not found: " + fileName);
+                e.printStackTrace();
+            }
 
-				
-				System.out.println(cone);
-				
-				
-				
-				sc.close();
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		 }
-		 
-	 }
-
+            for (Shape shape : shapes) {
+                // Display or perform operations based on the sorted shapes
+                System.out.println("Height: " + shape.getHeight() + ", Base Area: " + shape.getBaseArea() + ", Volume: " + shape.getVolume());
+            }
+        }
+    }
 }
