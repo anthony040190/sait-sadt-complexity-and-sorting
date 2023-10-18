421ebc4068632358c6a57653fa980b9057360fc2
JeanXristianPascua <119911259+JeanXristianPascua@users.noreply.github.com>
2023-10-18 01:03:16 -0600

    Add code

--- src/application/Driver.java
+++ src/application/Driver.java
@@ -1,8 +1,5 @@
 package application;
 
-import shape.Cone;
-import shape.Cylinder;
-import shape.Pyramid;
 import shape.*;
 
 import java.io.File;
@@ -17,8 +14,10 @@ public class Driver {
 	
 	 public static void main(String[] args) {
 		 
-//		 String[] fileNames = {"res/polyfor1.txt", "res/polyfor3.txt", "res/polyfor5.txt"};
-		 String[] fileNames = {"res/polyfor1.txt"};
+		 String[] fileNames = {"res/polyfor1.txt", "res/polyfor3.txt", "res/polyfor5.txt"};
+
+		 List<Shape> shapes = new ArrayList<>();
+		 List<Cone> cone = new ArrayList<>();
 		 
 		 for(String fileName : fileNames) {
 			 
@@ -32,13 +31,37 @@ public class Driver {
 					String data = sc.nextLine();
 					String[] splittedData = data.split(" ");
 					int numberOfData = Integer.parseInt(splittedData[0]);
+					String shapeType = splittedData[1];
+	                double height = Double.parseDouble(splittedData[2]);
 					
-					
-					System.out.println(numberOfData);
-					
+                    if (shapeType.equals("Cylinder")) {
+                        double radius = Double.parseDouble(splittedData[3]);
+                        shapes.add(new Cylinder(height, radius));
+                    } else if (shapeType.equals("Cone")) {
+                        double radius = Double.parseDouble(splittedData[3]);
+                        shapes.add(new Cone(height, radius));
+                    } else if (shapeType.equals("Pyramid")) {
+                        double side = Double.parseDouble(splittedData[3]);
+                        shapes.add(new Pyramid(height, side));
+                    } else if (shapeType.equals("PentagonalPrism")) {
+                        double side = Double.parseDouble(splittedData[3]);
+                        shapes.add(new Pyramid(height, side));
+                    } else if (shapeType.equals("TriangularPrism")) {
+                        double side = Double.parseDouble(splittedData[3]);
+                        shapes.add(new Pyramid(height, side));
+                    } else if (shapeType.equals("OctagonalPrism")) {
+                        double side = Double.parseDouble(splittedData[3]);
+                        shapes.add(new Pyramid(height, side));
+                    } else if (shapeType.equals("SquarePrism")) {
+                        double side = Double.parseDouble(splittedData[3]);
+                        shapes.add(new Pyramid(height, side));
+                    }
+  					
+				
 				}
+
 				
-				
+				System.out.println(cone);
 				
 				
 				
